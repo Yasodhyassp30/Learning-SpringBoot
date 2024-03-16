80e910590f18de19cab0137af326ca19756c9fa2
Yasodhya Perera <yasodhyapereras@gmail.com>
2024-03-16 12:05:52 +0530

    Refactor repository interfaces

--- src/main/java/com/example/demo/repository/projectRepository.java
+++ src/main/java/com/example/demo/repository/projectRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.models.projectModel;
 
 import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface projectRepository extends JpaRepository<projectModel, UUID> {
-    
+
+
 }
--- src/main/java/com/example/demo/repository/taskRepository.java
+++ src/main/java/com/example/demo/repository/taskRepository.java
@@ -7,8 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.models.taskModel;
 
 public interface taskRepository extends JpaRepository<taskModel, UUID>{
-    List<taskModel> findByProject(UUID project);
-    List<taskModel> findByAssigned(UUID assigned);
-    List<taskModel> findByStatus(String status);
-    List<taskModel> findByImportanceAndProject(UUID project, String importance);
+
 }
